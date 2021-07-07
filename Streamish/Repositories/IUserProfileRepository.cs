d4937e467617f3ca1675edfa967d254084dd3947
HPreast <preasths1@gmail.com>
2021-07-07 16:04:57 -0500

    Working on editing a user

---
+++ Streamish/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,12 @@
+﻿using Streamish.Models;
+using System.Collections.Generic;
+
+namespace Streamish.Repositories
+{
+    public interface IUserProfileRepository
+    {
+        void Add(UserProfile userProfile);
+        List<UserProfile> GetAllUsers();
+        void Update(UserProfile userProfile);
+    }
+}
